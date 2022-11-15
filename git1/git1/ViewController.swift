95040b796c607c9b6d4b74289105f294e98678ba
Dmitrii Shatov <shatovds@gmail.com>
2022-11-15 12:19:48 +0100

    Второй коммит

--- git1/git1/ViewController.swift
+++ git1/git1/ViewController.swift
@@ -8,9 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "Hello"
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        return label
+    }()
+    
+    private func addConstraints() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(label)
+        addConstraints()
         // Do any additional setup after loading the view.
     }
 
